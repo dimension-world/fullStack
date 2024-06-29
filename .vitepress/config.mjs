2f33cc1594e1ff6de1c23069250432a0cc55ae6d
高生 <321684721@qq.com>
2024-06-30 00:00:48 +0800

    打包出错

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -15,9 +15,9 @@ export default defineConfig({
     siteTitle: false,
     nav: [
       {
-        text:'前端',
-        items:[
-          {text:'JavaScript',link:'/src/JavaScript'},
+        text: '前端',
+        items: [
+          { text: 'JavaScript', link: '/src/JavaScript' },
           { text: 'Vue2', link: '/' },
           { text: 'VueRouter', link: '/' },
           { text: 'VueX', link: '/' },
@@ -25,11 +25,11 @@ export default defineConfig({
           { text: 'TypeScript', link: '/src/ts/' },
           { text: 'Pinia', link: '/src/pinia/' },
           { text: 'Vue3项目', link: '/src/project/index' },
-        ]
-      }, 
+        ],
+      },
       {
-        text:'后端',
-        items:[
+        text: '后端',
+        items: [
           { text: '待定', link: '/' },
           { text: '待定', link: '/' },
           { text: '待定', link: '/' },
@@ -37,8 +37,8 @@ export default defineConfig({
           { text: '待定', link: '/' },
           { text: '待定', link: '/' },
           { text: '待定', link: '/' },
-        ]
-      },     
+        ],
+      },
     ],
     sidebar: {
       '/src/JavaScript': [
@@ -65,47 +65,42 @@ export default defineConfig({
         {
           text: 'TypeScript',
           items: [
-            { text: 'TypeScript起步', link: '/src/ts/' }, 
+            { text: 'TypeScript起步', link: '/src/ts/' },
             { text: 'TypeScript核心', link: '/src/ts/core' },
             { text: 'TypeScript应用', link: '/src/ts/pro' },
             { text: 'TS补充', link: '/src/ts/backup' },
             { text: 'TS黑马头条案例', link: '/src/ts/case' },
-           
-          ]
-        }
+          ],
+        },
       ],
       '/src/pinia/': [
         {
           text: 'Pinia',
-          items: [
-            { text: 'Pinia 核心', link: '/src/pinia/' }, 
-          ]
-        }
+          items: [{ text: 'Pinia 核心', link: '/src/pinia/' }],
+        },
       ],
       '/src/project/': [
         {
           text: 'H5项目',
           items: [
-            { text: '1. 项目起步', link: '/src/project/' }, 
-            { text: '2. 登录模块', link: '/src/project/login' }, 
-            { text: '3. 用户模块', link: '/src/project/user' }, 
-            { text: '4. 首页模块', link: '/src/project/home' }, 
-            { text: '5. 极速问诊', link: '/src/project/consult' }, 
-            { text: '6. 医生问诊室', link: '/src/project/room' }, 
-            { text: '7. 问诊订单模块', link: '/src/project/consult-order' }, 
-            { text: '8. 药品订单模块', link: '/src/project/order' }, 
-            { text: '9. 其他扩展', link: '/src/project/end' }, 
+            { text: '1. 项目起步', link: '/src/project/' },
+            { text: '2. 登录模块', link: '/src/project/login' },
+            { text: '3. 用户模块', link: '/src/project/user' },
+            { text: '4. 首页模块', link: '/src/project/home' },
+            { text: '5. 极速问诊', link: '/src/project/consult' },
+            { text: '6. 医生问诊室', link: '/src/project/room' },
+            { text: '7. 问诊订单模块', link: '/src/project/consult-order' },
+            { text: '8. 药品订单模块', link: '/src/project/order' },
+            { text: '9. 其他扩展', link: '/src/project/end' },
             { text: '辅助-超级医生', link: '/src/project/super-doctor' },
             { text: '二次开发-开药问诊', link: '/src/project/drug' },
             { text: '二次开发-问医生', link: '/src/project/doctor' },
-          ]
-        }
+          ],
+        },
       ],
     },
 
-    socialLinks: [
-      { icon: 'github', link: '#' },
-    ],
+    socialLinks: [{ icon: 'github', link: '#' }],
     footer: {
       message: 'Released under the MIT License.',
       copyright: 'Copyright © 2024',
@@ -136,6 +131,19 @@ export default defineConfig({
     },
   },
   build: {
-    chunkSizeWarningLimit: 1600
-}
+    chunkSizeWarningLimit: 1600,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id
+              .toString()
+              .split('node_modules/')[1]
+              .split('/')[0]
+              .toString()
+          }
+        },
+      },
+    },
+  },
 })
