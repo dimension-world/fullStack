21e412bd98a1ab3bc10aef559281e41c2314c6b1
高生 <321684721@qq.com>
2024-06-30 00:03:11 +0800

    打包出错

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -131,9 +131,10 @@ export default defineConfig({
     },
   },
   build: {
-    chunkSizeWarningLimit: 1600,
+    chunkSizeWarningLimit: 1500, // 调整包的大小
     rollupOptions: {
       output: {
+        // 最小化拆分包
         manualChunks(id) {
           if (id.includes('node_modules')) {
             return id
@@ -143,6 +144,10 @@ export default defineConfig({
               .toString()
           }
         },
+        // 用于从入口点创建的块的打包输出格式[name]表示文件名,[hash]表示该文件内容hash值
+        entryFileNames: 'assets/js/[name].[hash].js', // 用于命名代码拆分时创建的共享块的输出命名
+        chunkFileNames: 'assets/js/[name].[hash].js', // 用于输出静态资源的命名，[ext]表示文件扩展名
+        assetFileNames: 'assets/[ext]/[name].[hash].[ext]',
       },
     },
   },
