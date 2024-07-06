8c515476fee8b3c4be01ec9f275476dc5e201c5d
rocket <321684721@qq.com>
2024-07-06 13:16:55 +0800

    NoSQL

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -36,7 +36,7 @@ export default defineConfig({
           { text: 'Pinia', link: '/src/pinia/' },
           { text: 'Vue3项目', link: '/src/project/' },
           { text: 'Koa', link: '/src/koa/' },
-          { text: 'NoSQL', link: '/src/nosql/' }
+          { text: 'NoSQL', link: '/src/NoSQL/' }
         ],
       },
       {
